4d4f0e015aa7618fb8faf0104ae02c6e5d07da5d
Ricardo Barbosa <rbrico@gmail.com>
2017-03-11 15:15:27 -0300

    Initial commit.

---
+++ Pitch Perfect/RecordingViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Ricardo Barbosa on 11/03/17.
+//  Copyright © 2017 Ricardo Barbosa. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordingViewController: UIViewController {
+  
+  // MARK: Properties
+  
+  var audioRecorder: AVAudioRecorder!
+  var filePath : URL?
+  
+  @IBOutlet weak var micButton: UIButton!
+  @IBOutlet weak var stopButton: UIButton!
+  @IBOutlet weak var label: UILabel!
+  
+  var recording : Bool = false {
+    didSet {
+      updateUI()
+    }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    updateUI()
+  }
+  
+  func updateUI() {
+    micButton.isEnabled = !recording
+    stopButton.isEnabled = recording
+    label.text = recording ? "Recording in progress" : "Tap to record"
+  }
+  
+  @IBAction func startRecording(_ sender: Any) {
+    recording = true
+    
+    let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+    let recordingName = "recordedVoice.wav"
+    let pathArray = [dirPath, recordingName]
+    filePath = URL(string: pathArray.joined(separator: "/"))
+    print(filePath ?? "?")
+    
+    let session = AVAudioSession.sharedInstance()
+    try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+    
+    try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+    audioRecorder.isMeteringEnabled = true
+    audioRecorder.prepareToRecord()
+    audioRecorder.record()
+    
+  }
+  
+  @IBAction func stopRecording(_ sender: Any) {
+    recording = false
+    audioRecorder.stop()
+    
+    let audioSession = AVAudioSession.sharedInstance()
+    try! audioSession.setActive(false)
+    
+    performSegue(withIdentifier: "EffectsViewController", sender: nil)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let vc = segue.destination as! EffectsViewController
+    vc.recordedAudioURL = filePath 
+  }
+  
+  
+}
+
